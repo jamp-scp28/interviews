f848119dcdc7690112023d188c201b5b64231385
jorgescp28 <jorge.molano28@gmail.com>
2020-06-10 22:52:03 -0500

    new

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',  # Enable the inner app 
-    'crispy_forms',
     'import_export',
     'interview',
     'django_filters',
