d128011a57efca10adc90ec1e222e659cfa73494
JorgeScp <45722406+JorgeScp@users.noreply.github.com>
2020-06-18 09:20:12 -0500

    update host

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,8 @@ DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'),
-'localhost', '192.168.1.3', config('SERVER', default='192.168.1.3')
+'localhost', '192.168.1.3', config('SERVER', default='192.168.1.3'),
+'hhrr20.pythonanywhere.com'
 ]
 
 # Application definition
